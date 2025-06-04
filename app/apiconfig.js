7c530ef146a76c89197dc5521b3e845e2c900b4d
sananabbasov <sanan_abbasov@list.ru>
2025-06-04 11:48:53 +0400

    tesa

--- app/apiconfig.js
+++ app/apiconfig.js
@@ -1,2 +1,4 @@
 // export const API_URL = "http://localhost:8080/api"
+// export const API_URL = "http://localhost:8080/api"
+// export const API_URL = "http://localhost:8080/api"
 export const API_URL = "http://167.99.132.197/api"
