7f46e7dcc7539ad668ddd9299916caf31f039431
sananabbasov <sanan_abbasov@list.ru>
2025-06-04 15:36:29 +0400

    tesa

--- app/apiconfig.js
+++ app/apiconfig.js
@@ -1,4 +1,4 @@
 // export const API_URL = "http://localhost:8080/api"
 // export const API_URL = "http://localhost:8080/api"
 // export const API_URL = "http://localhost:8080/api"
-export const API_URL = "http://167.99.132.197/api"
+export const API_URL = "https://onlaynders.az/api"
